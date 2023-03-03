cb17021e32d650bfaf69319ed98dc507986de034
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2023-03-03 15:39:42 +0100

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const HomePage = () => {
+    return;
+    <Fragment>
+        <Hero />
+        <FeaturedPosts />
+    </Fragment>;
+};
+
+export default HomePage;
+
+// header
+// hero section
+// featured posts
+// footer
---
+++ pages/posts/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllPostsPage = () => {
+  return (
+    <div>AllPostsPage</div>
+  )
+}
+
+export default AllPostsPage
